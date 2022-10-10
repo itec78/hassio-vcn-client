a3708b57c3fea8ea57189bec7f438599f73ae01f
itec <itec78@gmail.com>
2022-10-10 19:46:21 +0200

    vcn setup

--- tinc-vpn-client/run.sh
+++ tinc-vpn-client/run.sh
@@ -2,16 +2,8 @@
 
 CONFIG_PATH=/data/options.json
 
-SERVERNAME=$(bashio::config 'server_name')
-SERVERPUBLICKEY=$(bashio::config 'server_public_key')
-TINCSERVER=$(bashio::config 'connect_to')
-SERVERSUBNET=$(bashio::config 'server_subnet')
 CLIENTNAME=$(bashio::config 'client_name')
-PUBLICKEY=$(bashio::config 'public_key')
 PRIVATEKEY=$(bashio::config 'private_key')
-CLIENTSUBNET=$(bashio::config 'client_subnet')
-CLIENTROUTE=$(bashio::config 'client_route')
-ADDRESSFAMILY=$(bashio::config 'address_family')
 
 function init_tun(){
     mkdir -p /dev/net
@@ -20,57 +12,24 @@ function init_tun(){
     fi
 }
 
-function generate_private_key(){
-    tincd -n tinc0 -K4096
-}
-
 function generate_config_files(){
-    mkdir -p /etc/tinc/tinc0/hosts
+    mkdir -p /etc/tinc/vcn
+    echo ${PRIVATEKEY} | base64 -d > /etc/tinc/vcn/rsa_key.priv
+    chmod 600 /etc/tinc/vcn/rsa_key.priv
 
-    cat <<EOF > /etc/tinc/tinc0/tinc-up
-#!/bin/sh
-ip link set \$INTERFACE up
-ip addr add ${CLIENTSUBNET} dev \$INTERFACE
-ip route add ${CLIENTROUTE} dev \$INTERFACE
-EOF
-
-    cat <<EOF > /etc/tinc/tinc0/tinc-down
-#!/bin/sh
-ip route del ${CLIENTROUTE} dev \$INTERFACE
-ip addr del ${CLIENTSUBNET} dev \$INTERFACE
-ip link set \$INTERFACE down
-EOF
-
-    cat <<EOF > /etc/tinc/tinc0/tinc.conf
-Name = ${CLIENTNAME}
-AddressFamily = ${ADDRESSFAMILY}
-Interface = tinc0
-ConnectTo = ${SERVERNAME}
-EOF
-
-    cat <<EOF > /etc/tinc/tinc0/hosts/${CLIENTNAME}
-Subnet = ${CLIENTSUBNET}
-
-
-EOF
-
-    cat <<EOF > /etc/tinc/tinc0/hosts/${SERVERNAME}
-Subnet = ${SERVERSUBNET}
-Address = ${TINCSERVER}
-
-
-EOF
-
-    echo ${PUBLICKEY} | base64 -d >> /etc/tinc/tinc0/hosts/${CLIENTNAME}
-    echo ${SERVERPUBLICKEY} | base64 -d >> /etc/tinc/tinc0/hosts/${SERVERNAME}
-    echo ${PRIVATEKEY} | base64 -d > /etc/tinc/tinc0/rsa_key.priv
-
-    chmod 600 /etc/tinc/tinc0/rsa_key.priv
-    chmod +x /etc/tinc/tinc0/tinc-up
-    chmod +x /etc/tinc/tinc0/tinc-down
+    cd /etc/tinc # or your tinc configuration dir
+    mkdir -p vcn/hosts/
+    curl -o vcn/hosts/${CLIENTNAME} https://attrezzi.esiliati.org/vcn//host/${CLIENTNAME}
+    curl -o vcn/hosts/vcn1 https://attrezzi.esiliati.org/vcn//hosts/vcn1
+    curl -o vcn/hosts/vcn2 https://attrezzi.esiliati.org/vcn//hosts/vcn2
+    curl -o vcn/hosts/vcn3 https://attrezzi.esiliati.org/vcn//hosts/vcn3
+    curl -o vcn/tinc-up https://attrezzi.esiliati.org/vcn//tinc-up/${CLIENTNAME}
+    chmod +x vcn/tinc-up
+    curl -o vcn/tinc.conf https://attrezzi.esiliati.org/vcn//tinc.conf/${CLIENTNAME} 
+    
 }
 
 init_tun
 generate_config_files
 
-tincd -n tinc0 -D -d
+tincd -n vcn -D -d
